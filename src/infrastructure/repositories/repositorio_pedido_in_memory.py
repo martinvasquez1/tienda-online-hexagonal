2b6252dae9ab916d6c4f938e7a9a00272a361e70
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 23:04:17 -0400

    feat: repositorio pedido in memory

---
+++ src/infrastructure/repositories/repositorio_pedido_in_memory.py
@@ -0,0 +1,19 @@
+from src.domain.repositories.repositorio_pedido import RepositorioPedido
+from src.domain.entities.Pedido import Pedido
+
+
+class RepositorioPedidoInMemory(RepositorioPedido):
+    def agregar(self, pedido: Pedido) -> None:
+        return "Este método agregará un nuevo pedido al repositorio."
+
+    def obtener(self, id_pedido) -> Optional[Pedido]:
+        return "Este método obtendrá un pedido por su ID."
+
+    def listar(self) -> List[Pedido]:
+        return "Este método listará todos los pedidos en el repositorio."
+
+    def actualizar(self, pedido: Pedido) -> None:
+        return "Este método actualizará la información de un pedido existente."
+
+    def eliminar(self, id_pedido) -> None:
+        return "Este método eliminará un pedido del repositorio por su ID."
