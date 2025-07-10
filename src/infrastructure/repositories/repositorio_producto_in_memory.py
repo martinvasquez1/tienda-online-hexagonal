4c728f7697de8fce4ed3705af19496fcc460eb0e
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 23:03:40 -0400

    feat: repositorio producto in memory

---
+++ src/infrastructure/repositories/repositorio_producto_in_memory.py
@@ -0,0 +1,19 @@
+from src.domain.repositories.repositorio_producto import RepositorioProducto
+from src.domain.entities.Producto import Producto
+
+
+class RepositorioProductoInMemory(RepositorioProducto):
+    def agregar(self, producto: Producto) -> None:
+        return "Este método agregará un nuevo producto al repositorio."
+
+    def obtener(self, id_producto) -> Optional[Producto]:
+        return "Este método obtendrá un producto por su ID."
+
+    def listar(self) -> List[Producto]:
+        return "Este método listará todos los productos en el repositorio."
+
+    def actualizar(self, producto: Producto) -> None:
+        return "Este método actualizará la información de un producto existente."
+
+    def eliminar(self, id_producto) -> None:
+        return "Este método eliminará un producto del repositorio por su ID."
