0dd89b2d94ca19c7ed02659a2a7f94dba075b92a
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 22:35:37 -0400

    feat: esqueleto del servicio de pedidos

---
+++ src/application/pedido_servicio.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+from uuid import UUID
+
+from src.application.pedido_servicio_port import PedidoServiceBase
+from src.domain.entities.Pedido import Pedido
+
+
+class PedidoService(PedidoServiceBase):
+    def registrar_pedido(self, productos) -> Optional[Pedido]:
+        return "Este método registrará un nuevo pedido y devolverá el objeto Pedido creado."
+
+    def listar_todos_los_pedidos(self) -> List[Pedido]:
+        return "Este método devolverá una lista de todos los pedidos registrados."
+
+    def buscar_pedido_por_id(self, id: UUID) -> Optional[Pedido]:
+        return "Este método buscará un pedido por su ID y devolverá el objeto Pedido si se encuentra."
+
+    def eliminar_pedido(self, id: UUID) -> bool:
+        return "Este método eliminará un pedido por su ID y devolverá True si se eliminó con éxito."
