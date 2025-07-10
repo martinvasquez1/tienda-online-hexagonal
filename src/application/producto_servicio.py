3c03ebfb9a3798f62b17dc9e48f04b2b0ced4175
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2025-07-09 22:37:35 -0400

    feat: esqueleto del servicio de productos

---
+++ src/application/producto_servicio.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from src.domain.entities.producto import Producto
+from src.application.producto_servicio_port import ProductoServiceBase
+
+
+class ProductoService(ProductoServiceBase):
+    def registrar_producto(self, precio: float) -> Optional[Producto]:
+        return "Este método registrará un nuevo producto y devolverá el objeto Producto creado."
+
+    def listar_todos_los_productos(self) -> List[Producto]:
+        return "Este método devolverá una lista de todos los productos registrados."
+
+    def buscar_producto_por_id(self, id: int) -> Optional[Producto]:
+        return "Este método buscará un producto por su ID y devolverá el objeto Producto si se encuentra."
+
+    def eliminar_producto(self, id: int) -> bool:
+        return "Este método eliminará un producto por su ID y devolverá True si se eliminó con éxito."
