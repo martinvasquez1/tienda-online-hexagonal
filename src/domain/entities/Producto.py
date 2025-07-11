8dcff392ee2c7d5bb681686725ed7e0960577225
Catrilao <87039539+Catrilao@users.noreply.github.com>
2025-07-11 00:47:20 -0400

    fix: agregar stock

--- src/domain/entities/Producto.py
+++ src/domain/entities/Producto.py
@@ -1,5 +1,6 @@
 class Producto:
-    def __init__(self, id, nombre, precio):
+    def __init__(self, id, nombre, precio, stock):
         self.id = id
         self.nombre = nombre
         self.precio = precio
+        self.stock = stock
