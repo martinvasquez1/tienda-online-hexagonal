615ac7b6eebdfe807ec1d4e4f9f031f5154986e5
Catrilao <87039539+Catrilao@users.noreply.github.com>
2025-07-11 10:48:37 -0400

    fix: respuesta vacia

--- src/infrastructure/http_handlers/cliente_handler.py
+++ src/infrastructure/http_handlers/cliente_handler.py
@@ -41,13 +41,7 @@ async def obtener_clientes(
     try:
         clientes = servicio_cliente.obtener_clientes()
 
-        if clientes is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Lista de clientes no encontrada",
-            )
-
-        return clientes
+        return clientes or []
 
     except Exception as e:
         raise HTTPException(
@@ -65,12 +59,7 @@ async def obtener_cliente(
     try:
         cliente = servicio_cliente.obtener(cliente_id)
 
-        if cliente is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Cliente no encontrado"
-            )
-
-        return cliente
+        return cliente or []
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -94,13 +83,8 @@ async def actualizar_cliente(
             datos.tipo,
         )
 
-        if cliente is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="No se pudo actualizar el cliente",
-            )
+        return cliente or []
 
-        return cliente
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
